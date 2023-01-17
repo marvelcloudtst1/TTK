378916e4153faa8e96d03f1815d8daf390144b5a
marvelcloudtst1 <122812509+marvelcloudtst1@users.noreply.github.com>
2023-01-17 14:04:36 +0530

    Update admin_check.py

--- tortoolkit/functions/admin_check.py
+++ tortoolkit/functions/admin_check.py
@@ -25,32 +25,5 @@ async def is_admin(client, user_id, chat_id, force_owner=False):
             return True
         else:
             return False
-
-    try:
-        res = await client(GetParticipantRequest(channel=chat_id, user_id=user_id))
-
-        try:
-            if isinstance(
-                res.participant,
-                (
-                    ChannelParticipantAdmin,
-                    ChannelParticipantCreator,
-                    ChannelParticipantsAdmins,
-                ),
-            ):
-                return True
-            else:
-
-                if user_id in get_val("ALD_USR"):
-                    return True
-                else:
-                    return False
-        except:
-            torlog.info("Bot Accessed in Private {}".format(traceback.format_exc()))
-            return False
-    except Exception as e:
-        torlog.info("Bot Accessed in Private {}".format(e))
-        if user_id in get_val("ALD_USR"):
-            return True
-        else:
-            return False
+    else:
+        return True
