0b86c49a24189f71e9c84e9f347225a0d996fcf8
marvelcloudtst1 <122812509+marvelcloudtst1@users.noreply.github.com>
2023-01-17 14:00:10 +0530

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -6,17 +6,17 @@ except:
         # Set true if its VPS
         IS_VPS = False
 
-        API_HASH = ""
-        API_ID = 0
-        BOT_TOKEN = ""
-        BASE_URL_OF_BOT = ""
+        API_HASH = "d28109d5fd13dd9c9e87c90e8c4ff7c5"
+        API_ID = 5401122
+        BOT_TOKEN = "5866956084:AAHIgCnHP9Z0kVA0Td0T2nfKpT25bYVo5Rg"
+        BASE_URL_OF_BOT = "starkai.live"
 
         # Edit the server port if you want to keep it default though.
         SERVPORT = 80
 
         # ALLOWED USERS [ids of user or supergroup] seperate by commas
-        ALD_USR = []
-        OWNER_ID = 0
+        ALD_USR = [-1001818533677]
+        OWNER_ID = 2141745029
 
         # Google Drive Index Link should include the base dir also See readme for more info
         GD_INDEX_URL = False
@@ -38,7 +38,7 @@ except:
 
         # DB URI for access
         DATABASE_URL = (
-            "dbname=tortk user=postgres password=your-pass host=127.0.0.1 port=5432"
+            "postgresql://postgres:YU3CHqV1U8PHr9hr8qhW@containers-us-west-30.railway.app:6443/railway"
         )
 
         # UNCOMMENT THE BELOW LINE WHEN USING CONTAINER AND COMMENT THE UPPER LINE
@@ -75,10 +75,10 @@ except:
         DEF_RCLONE_DRIVE = ""
 
         # Max size of a playlist that is allowed (Number of videos)
-        MAX_YTPLAYLIST_SIZE = 20
+        MAX_YTPLAYLIST_SIZE = 2000
 
         # Max size of the torrent allowed
-        MAX_TORRENT_SIZE = 10
+        MAX_TORRENT_SIZE = 1000
 
         # Custom Trackers for QBT..
         ADD_CUSTOM_TRACKERS = True
